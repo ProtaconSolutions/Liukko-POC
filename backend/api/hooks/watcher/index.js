b36d77173fed4af5d63f52c73016ee0e0c64335b
tarlepp <tarmo.leppanen@kase.fi>
2014-12-15 21:45:14 +0200

    Refactored watcher hook to use file data from database.

--- backend/api/hooks/watcher/index.js
+++ backend/api/hooks/watcher/index.js
@@ -5,42 +5,77 @@ var chokidar = require('chokidar');
 var fs = require('fs');
 var _ = require('lodash');
 
-module.exports = function watcherHook(sails) {
+/**
+ * Watcher hook.
+ *
+ * This file contains a custom hook, that will be handling actual "watching" of specified files and if those changes
+ * hook will emit a socket message about that change to every client who is "listening" file specified socket room.
+ *
+ * @todo
+ *  1) How to handle new files?
+ *  2) Figure out how to wait for 'initializer' hook
+ */
+module.exports = function hook(sails) {
     return {
-        // Default configuration for hook
-        defaults: function defaults() {
-            /**
-             * Return the default configuration, which will be merged into the Sails config if not overridden by the
-             * user.
-             */
-            return {
-                watcherHook: {
-                    files: []
-                }
-            };
+        /**
+         * Hook initialize.
+         *
+         * @param   {Function}  next    Callback function to call after all is done
+         */
+        initialize: function initialize(next) {
+            var hook = this;
+            var eventsToWaitFor = [];
+
+            if (sails.hooks.orm) {
+                eventsToWaitFor.push('hook:orm:loaded');
+            }
+
+            if (sails.hooks.sockets) {
+                eventsToWaitFor.push('hook:sockets:loaded');
+            }
+
+            sails.after(eventsToWaitFor, function() {
+                hook._determineFilesToWatch(next);
+            });
         },
 
         /**
-         * Initialize the hook
+         * Private hook function to determine all files to watch with this hook.
          *
-         * @param  {Function}   next    Callback for when we're done initializing
+         * @param   {Function}  next    Callback function to call after all is done
+         * @private
          */
-        initialize: function initialize(next) {
-            // If the hook has been deactivated, just return
-            if (sails.config.watcherHook.files.length === 0) {
-                sails.log.verbose("POC file listener hook deactivated. No files configured at this moment...");
+        _determineFilesToWatch: function _determineFilesToWatch(next) {
+            var hook = this;
 
-                return next();
-            }
+            sails.models['file']
+                .find()
+                .exec(function exec(error, files) {
+                    if (error) {
+                        next(error);
+                    } else {
+                        hook._watchFiles(files, next);
+                    }
+                });
+        },
 
-            // Normalize files to watch, basically just add some base path to each files
-            var filesToWatch = _.pluck(sails.config.watcherHook.files, 'path');
-
-            // Watch all specified files
-            var watcher = chokidar.watch(filesToWatch, {
-                // Ignore the initial "add" events which are generated when Chokidar starts watching files
+        /**
+         * Private hook method to activate actual watcher.
+         *
+         * @param   {Array}     files   Array of files that are going to be watched
+         * @param   {Function}  next    Callback function to call after all is done
+         * @private
+         */
+        _watchFiles: function _watchFiles(files, next) {
+            /**
+             * Ignore the initial "add" events which are generated when Chokidar starts watching files. Also we need
+             * to make persistent check of these files. And we need to use polling to get all updates from certain
+             * files.
+             */
+            var watcher = chokidar.watch(_.pluck(files, 'path'), {
                 ignoreInitial: true,
-                persistent: true
+                persistent: true,
+                usePolling: true
             });
 
             /**
@@ -48,19 +83,19 @@ module.exports = function watcherHook(sails) {
              * after receiving all of the change events.
              */
             watcher.on('change', sails.util.debounce(function debounce(file, stats) {
-                sails.log.verbose('Detected POC file change -- ' + path);
+                sails.log.verbose('Detected POC file change -- ' + file);
 
                 // Read changed file contents
                 fs.readFile(file, 'utf8', function callback(error, data) {
                     if (error) {
-                        sails.log.error('File read error on POC hook - ' + path);
+                        sails.log.error('File read error on POC hook - ' + file);
                         sails.log.error(error);
                     } else {
                         sails.log.verbose('File changed content is now:');
                         sails.log.verbose(data);
 
                         // Determine file configuration
-                        var fileConfig = _.find(sails.config.watcherHook.files, function iterator(_file) {
+                        var fileConfig = _.find(files, function iterator(_file) {
                             return _file.path === file;
                         });
 
@@ -69,7 +104,7 @@ module.exports = function watcherHook(sails) {
                             sails.sockets.emit(
                                 sails.sockets.subscribers(fileConfig.room),
                                 fileConfig.room,
-                                {data: data, stats: stats}
+                                {contents: data, stats: stats}
                             );
                         } else {
                             sails.log.error('File configuration not found for file - ' + file);
@@ -78,8 +113,7 @@ module.exports = function watcherHook(sails) {
                 });
             }, 100));
 
-            // We're done initializing.
-            return next();
+            next();
         }
     };
 };
